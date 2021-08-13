23a302883d2e92909eed08dbe9cc34fbc2696ce3
Jhonyyyyy <88861091+Jhonyyyyy@users.noreply.github.com>
2021-08-13 17:55:40 +0900

    Update bitcoinAutoTradeWithAI.py

--- bitcoinAutoTradeWithAI.py
+++ bitcoinAutoTradeWithAI.py
@@ -5,9 +5,9 @@ import schedule
 import requests
 from fbprophet import Prophet
 
-access = "dCHhpFD2wGuxGkEybEOrEZg1ogUwXMRvWNx5pvzE"
-secret = "xWgUlKSbWzxHgZrJ0JEHMtgxSOMS6yz44aHsP2nF"
-myToken = "xoxb-2382932486212-2389364946769-9K7sssGQyJ6adnKRusLUWDKT"
+access = "dCHhpFD2wGuxGkEybEOrEZg1ogUwXMR"
+secret = "xWgUlKSbWzxHgZrJ0JEHMtgxSOMS6yz"
+myToken = "xoxb-2382932486212-2389364946769-9K"
 
 def post_message(token, channel, text):
     """슬랙 메시지 전송"""
